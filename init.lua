d26b1bc978407854fb1cdb1dcc0ee654ec1a7803
0siribix <nomail@me.pl>
2021-10-03 22:03:11 -0500

    Added locale folder and template file. Cleaned up and commented code. Added bone drop for dmobs:skeleton. Few other spelling corrections or old comment removal.

--- init.lua
+++ init.lua
@@ -1,9 +1,17 @@
---[[ To Do:
-Add tool wear and repair
+--[[
+
+Blink for Minetest
+
+Code is licensed under the GNU LGPL version 3 or later.
+See LICENSE.txt and http://www.gnu.org/licenses/lgpl-3.0.txt
+
+
+To Do:
 Localize
 Maybe switch to first weapon in hotbar when blinking behind players or mobs
-Clean up and document code
-Put messages in HUD instead of in chat
+Maybe put messages in HUD instead of in chat
+Add a check for destination when raycast doesn't intersect
+	It may be possible to glitch through a single layer of nodes if player uses blink at maximum distance since we move the Y - 0.5
 ]]
 
 
@@ -43,6 +51,8 @@ local double_tap_tp = core.settings:get("blink:double_tap_tp") or true
 local num_uses = core.settings:get("blink:num_uses") or 150
 
 
+-- You can easily add to this list externally
+-- Simply add blink as an optional dependency and do blink.valid_entities["modname"] = true
 blink.valid_entities = {
 	["mobs_animal"] = true,
 	["mobs_monster"] = true,
@@ -58,6 +68,8 @@ blink.valid_entities = {
 	["dmobs"] = true
 }
 
+-- This is number of times to loop through the colors in blink_spectrum.png
+-- display time of each color is display_time / (number of colors * color_loops)
 local color_loops = 2	-- If display_time is short this the colors will cycle quickly. If it is long then they will cycle very slowly
 local tool_wear = math.ceil(65536 / num_uses)
 
@@ -72,9 +84,13 @@ function display_marker(user, itemstack, marker)
 	blink_tp(user, itemstack, marker)
 end
 
+-- This function is available to other mods
+-- Marker should be nil if you want to tp the player or a string name of a registered entity to display
+-- Currently blink:marker2 is hardcoded but this could be easily changed if someone wanted to have that functionality in the future
+-- if itemstack is not nil then add wear and return that itemstack
 function blink_tp(user, itemstack, marker)
+
 	local username = user:get_player_name()
-	local markername = "blink:marker"	-- change this to 2 if we can't tp to this destination
 	if not marker then
 		if blink.cooldown[username] then
 			core.chat_send_player(username,
@@ -83,6 +99,7 @@ function blink_tp(user, itemstack, marker)
 		end
 	end
 
+	local markername = marker or "blink:marker"	-- change this to 2 if we can't tp to this destination
 	local origin = user:get_pos()
 	local yaw	-- use these if we move behind a player or mob
 	local reset_pitch = false
@@ -97,22 +114,27 @@ function blink_tp(user, itemstack, marker)
 
 	local dir = user:get_look_dir()
 	local dpos = table.copy(origin)
+	-- It's possible to use vector.add but this is a fairly new feature and waasn't compatible with Multicraft servers at the time of development
+	-- The builtin function is written in LUA so there is no performance loss by doing it here manually
 	dpos.x = dpos.x + (dir.x * blink_distance)
 	dpos.y = dpos.y + (dir.y * blink_distance)
 	dpos.z = dpos.z + (dir.z * blink_distance)
 
 	local no_space_to_blink = false
+	-- Create a ray from player's eyes to blink_distance in the direction player is looking
 	local rc = Raycast(origin, dpos, true, false)
 
 	-- When I first wrote this code, I thought that all this calculation might cause server lag but it actually does very well
+	-- Loop through objects that the ray intersects with
 	for pt in rc do
 		if blink_behind and pt.type == "object" then
 			if pt.ref ~= user then		-- Raycast intersects with players head first
-				ref_ent = pt.ref:get_luaentity()
+				local ref_ent = pt.ref:get_luaentity()
 				if pt.ref:is_player() or (
 						ref_ent and
 				         blink.valid_entities[ref_ent.name:split(":")[1]]) then
 					local npos = pt.ref:get_pos()
+					-- It took LOTS of testing and crashing to get this to work correctly
 					if pt.ref:is_player() then
 						yaw = pt.ref:get_look_horizontal()
 					else
@@ -126,16 +148,18 @@ function blink_tp(user, itemstack, marker)
 					dpos.x = npos.x - (lookdir.x * 2)
 					dpos.z = npos.z - (lookdir.z * 2)
 					dpos.y = npos.y
-					npos.y = npos.y + 1		-- cast the ray at an angle to hopefully catch edge case scenarios
+					-- The purpose of adding one here is because we are checking for any blockage between the player/mob and the blink destination
+					-- It's possible that checking a straight line would allow someone to blink behind the target where there is not enough room to blink otherwise
+					-- By adding 1 to the Y, we are now casting a ray at an angle which should only allow to blink as long as it's clear two blocks tall
+					npos.y = npos.y + 1
 					if Raycast(npos, dpos, false, false):next() then no_space_to_blink = true end
-					break
-				--else	-- other entity
-				--	core.chat_send_player(username, tostring(ref_ent.name))
+					break	-- At this point, the raycast has intersected with a player or mob and there is no reason to keep looking further
 				end
 			end
 		elseif pt.type == "node" then
 			local npos = core.get_pointed_thing_position(pt)
 			local n = core.get_node(npos).name
+			-- First check for unknown node, then see if node is walkable
 			if core.registered_nodes[n] == nil or core.registered_nodes[n].walkable then
 				dpos.x = npos.x + pt.intersection_normal.x
 				dpos.y = npos.y + pt.intersection_normal.y
@@ -145,8 +169,10 @@ function blink_tp(user, itemstack, marker)
 		end
 	end
 
+	-- Save marker pos to oldpos and compare location to see whether we need to blink to the current dpos or create a new marker
 	local oldpos
 
+	-- Remove marker if there is one
 	if blink.active_marker[username] then
 		if double_tap_tp and marker then
 			oldpos = blink.active_marker[username]:get_pos()
@@ -155,7 +181,7 @@ function blink_tp(user, itemstack, marker)
 		blink.active_marker[username] = nil
 	end
 
-	if not tp_into_prot and core.is_protected(dpos, username)  and core.is_protected(origin, public_username) then
+	if not tp_into_prot and core.is_protected(dpos, username) and core.is_protected(origin, public_username) then
 		core.chat_send_player(username, S("Cannot blink into protected areas"))
 		return
 	else
@@ -181,14 +207,18 @@ function blink_tp(user, itemstack, marker)
 		end
 	end
 
+	-- if oldpos is set then that means we just removed a marker from this pos
+	-- if oldpos == dpos then we should blink there instead of creating a new marker
 	if (not no_space_to_blink) and oldpos and math.floor(oldpos.x) == math.floor(dpos.x) and
 				math.floor(oldpos.y) == math.floor(dpos.y) and
 				math.floor(oldpos.z) == math.floor(dpos.z) then
 		marker = nil
 	end
 
+	-- if marker is not nil then we need to create a marker at dpos, otherwise blink to dpos
 	if marker == nil then
 		if not no_space_to_blink then
+			-- When we set_pos, the player's feet is set to the Y level. Move down 0.5 so we don't fall half a block every time we blink
 			dpos.y = dpos.y - 0.5
 			user:set_pos(dpos)
 			core.sound_play("blink_swoosh", {pos = dpos, max_hear_distance = 10})
@@ -196,7 +226,7 @@ function blink_tp(user, itemstack, marker)
 			if reset_pitch then user:set_look_vertical(0) end
 			if not core.is_creative_enabled(username) then
 				local cooldown = cooldown_base +
-					cooldown_factor * vector.distance(origin, dpos)
+						cooldown_factor * vector.distance(origin, dpos)
 				blink.cooldown[username] = true
 				core.after(cooldown, function() blink.cooldown[username] = false end)
 			end
@@ -204,6 +234,7 @@ function blink_tp(user, itemstack, marker)
 	else
 		blink.active_marker[username] = core.add_entity(dpos, markername, username)
 	end
+
 	if itemstack then
 		itemstack:add_wear(tool_wear)
 		return itemstack
@@ -239,6 +270,7 @@ core.register_tool("blink:forever_rune", {
 		return blink_tp(user)
 		end
 })
+
 e_def = {
 	physical = false,
 	collisionbox = {0,0,0,0,0,0},
@@ -346,9 +378,11 @@ else
 			end
 		end
 	end
-
-	----------------- figure out how to add bone drop for skeleton mob
 end
+
+-- If bones is not installed and we have dmobs:skeleton, add Blink:bone to skeleton drops
+local skel = core.registered_entities["dmobs:skeleton"]
+if skel then skel.drops = {name = "blink:bone_shard", chance = 2, min = 2, max = 5} end
 
 --     Register Rune Crafts     --
 local ing1, ing2 = "", ""
